bae11734831fc5cdb07b0265eaae5ba47d848fb0
Mr. Crazy Unknown <suchiv2311@gmail.com>
2021-01-22 23:04:37 +0530

    Divide or Subtract

---
+++ 1451A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, n;
+    cin>>n;
+    while(n--)
+    {
+        cin>>i;
+        if(i==1)
+        {
+            cout<<0<<endl;
+        }
+        else if(i==2)
+        {
+            cout<<1<<endl;
+        }
+        else if(i==3)
+        {
+            cout<<2<<endl;
+        }
+        else if(i%2==0)
+        {
+            cout<<2<<endl;
+        }
+        else
+        {
+            cout<<3<<endl;
+        }
+    }
+    return 0;
+}

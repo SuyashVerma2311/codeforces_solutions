c8fd13903cb38b485bfb20773ecaf151b6e170c0
SuyashVerma2311 <suchiv2311@gmail.com>
2022-01-05 11:39:33 +0530

    Perfect Permutation

---
+++ 233A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin>>n;
+    if(n%2) {
+        cout<<"-1\n";
+        return 0;
+    }
+    for(int i=0; i<n; i++) {
+        if(i%2==0)
+            cout<<i+2<<" ";
+        else
+            cout<<i<<" ";
+    }
+    cout<<endl;
+}
